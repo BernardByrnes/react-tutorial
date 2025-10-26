ab90cb49af2fb831a9c8d3db8ab58dbad15a9b37
BernardByrnes <bernardtamboedu@gmail.com>
2025-10-26 16:11:28 +0300

    day 2

--- react-smilda/my/src/index.js
+++ react-smilda/my/src/index.js
@@ -2,43 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const author = "Mel Robbins";
-const title = "The Let Them Theory";
-const img = "./images/image-1.jpg";
-
-const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "./images/book-1.jpg",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-};
-const thirdBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "./images/book-1.jpg",
-};
+const books = [
+  {
+    author: "Mel Robbins",
+    title: "The Let Them Theory",
+    img: "./images/image-1.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/81kg51XRc1L._AC_UF1000,1000_QL80_.jpg",
+    id: 2,
+  },
+  {
+    author: " John Grisham",
+    title: "The Widow",
+    img: "https://i.ebayimg.com/images/g/vnEAAeSwVsdo6Wsm/s-l1600.jpg",
+    id: 3,
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author, children }) => {
+  // console.log(props);
+  // const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author.toUpperCase()}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      {children}
     </article>
   );
 };
