154cd88269cc3502ac94178184cc6a087c821656
BernardByrnes <bernardtamboedu@gmail.com>
2025-10-28 21:22:09 +0300

    day4

--- react-smilda/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ react-smilda/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
+import React, { useState } from "react";
+import { people } from "../../../data";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState("Reading books");
+
+  const displayPerson = () => {
+    setName("John Wick");
+    setAge(28);
+    setHobby("Kicking Ass");
+  };
+
+  console.log(name, age, hobby);
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>Enjoys : {hobby}</h3>
+      <button type="button" onClick={displayPerson}>
+        Show John
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
