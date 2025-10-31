bc0e818c83bbfba8006c3ea9934c1e420bfeaf50
BernardByrnes <bernardtamboedu@gmail.com>
2025-10-31 23:12:54 +0300

    day5

--- react-smilda/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ react-smilda/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,33 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+      <h3>Github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          console.log(user);
+          return <li key={user.id}></li>;
+        })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
