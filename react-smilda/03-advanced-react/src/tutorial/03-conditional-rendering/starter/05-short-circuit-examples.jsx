0b0dc602f7856cba44708bee7990cbfd3eb46591
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-04 23:39:12 +0300

    multiple fetch

--- react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
+  const [name, setName] = useState("susan");
+  const [user, setUser] = useState({ name: "john" });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      <h2>{name || "Ok its me again"}</h2>
+    </div>
+  );
 };
 
 export default ShortCircuitExamples;
