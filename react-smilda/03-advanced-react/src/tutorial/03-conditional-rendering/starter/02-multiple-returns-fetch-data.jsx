24288ac95659e421b8cfc743f93606a35e06c80f
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-04 17:56:35 +0300

    useeffect fetch1

--- react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,57 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        const userData = await response.json();
+        setUser(userData);
+        setIsLoading(false);
+        console.log(userData);
+      } catch (error) {
+        setIsError(true);
+        console.log(error.message);
+      }
+    };
+
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Is loading</h2>;
+  }
+
+  if (isError) {
+    return <h2>There was an error</h2>;
+  }
+
+  // const { avatar_url, name, company, bio } = user;
+
+  return (
+    <section>
+      <h2>Fetch Data</h2>;
+      <img
+        style={{ width: "150px", borderRadius: "50%" }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <h4>Works at {user.company}</h4>
+      <p>{user.bio}</p>
+    </section>
+  );
 };
+
 export default MultipleReturnsFetchData;
