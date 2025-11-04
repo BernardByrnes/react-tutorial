5ab60efc0ff8a1d8fd55317f473cdf42beb32684
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-04 23:05:05 +0300

    multiple fetch

--- react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState("");
+  const [name, setName] = useState("susan");
+
+  return (
+    <div>
+      <h4>Falsy Or: {text || "hello world"}</h4>;
+      <h4>Falsy Or: {text && "hello world"}</h4>;
+      <h4>Truthy Or: {name || "hello world"}</h4>;
+      <h4>Truthy Or: {name && "hello world"}</h4>;
+    </div>
+  );
 };
 export default ShortCircuitOverview;
