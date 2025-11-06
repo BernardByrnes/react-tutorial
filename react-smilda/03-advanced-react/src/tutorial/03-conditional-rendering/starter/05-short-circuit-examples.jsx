b52f77c39bca4ec1d1c09c3b4280c53b199871f1
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-06 18:44:09 +0300

    day8

--- react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -10,7 +10,25 @@ const ShortCircuitExamples = () => {
 
   return (
     <div>
-      <h2>{name || "Ok its me again"}</h2>
+      <h2>{text || "Ok its me again"}</h2>
+      {text && (
+        <div>
+          <h2>Its Me</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {user && <SomeComp name={user.name} />}
+      <h2 style={{ marginBlock: "1rem" }}>Ternary Operator</h2>
+      <button className="btn">{isEditing ? "Edit" : "Add"}</button>
+    </div>
+  );
+};
+
+const SomeComp = ({ name }) => {
+  return (
+    <div>
+      <h2>AM the conditional Guy Man</h2>
+      <h2>My name is {name}</h2>
     </div>
   );
 };
--- react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ react-smilda/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: "Bread Maker" });
+  };
+
+  const logOut = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello There,{user.name}</h4>
+          <button className="btn" onClick={logOut}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>kale</h4>
+          <button className="btn" onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
