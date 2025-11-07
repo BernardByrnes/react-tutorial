bda0925fb45138c9d2c73f4347e98d253d311877
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-07 09:34:14 +0300

    day8

--- react-smilda/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ react-smilda/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CleanupFunction = () => {
-  const [toggle, setToogle] = useState(false);
-
+  const [toggle, setToggle] = useState(false);
+  console.log("Render");
   return (
     <div>
-      <button
-        className="btn"
-        onClick={() => {
-          setToogle(!toggle);
-        }}
-      >
+      <button className="btn" onClick={() => setToggle(!toggle)}>
         toggle component
       </button>
-      {toggle && <Randcomp />}
+      {toggle && <RandomComponent />}
     </div>
   );
 };
+const RandomComponent = () => {
+  useEffect(() => {
+    const someFunc = () => {
+      //some logic
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => removeEventListener("scroll");
+  }, []);
 
-const Randcomp = () => {
-  return <h1>Heyy daya</h1>;
+  return <h1>hello there</h1>;
 };
-
 export default CleanupFunction;
