d703747642f107542e4fc10d224621ae68963803
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-24 22:30:46 +0300

    update

--- pro-academy/src/app.js
+++ pro-academy/src/app.js
@@ -1,5 +1,14 @@
+import "./app.css";
+
 function App() {
-  return <h2>This is First component</h2>;
+  let msg = "Awesome Bro";
+  return (
+    <div>
+      <h1 className="myHeader">Welcome to my app</h1>
+      <p className="myParagraph">This is First component {msg}!!! </p>
+      <input className="myInput" type="text" placeholder="hello" />
+    </div>
+  );
 }
 
 export default App;
