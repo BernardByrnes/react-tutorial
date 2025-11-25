f1beb731014cf4ad143944eadf627ab3becf4597
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-25 11:03:14 +0300

    update

--- pro-academy/src/app.js
+++ pro-academy/src/app.js
@@ -2,13 +2,11 @@ import "./app.css";
 import Products from "./Components/Products";
 
 function App() {
-  let msg = "Awesome Bro";
   return (
-    <div>
-      <h1 className="myHeader">Welcome to my app</h1>
-      <p className="myParagraph">This is First component {msg}!!! </p>
-      <input className="myInput" type="text" placeholder="hello" />
-      <Products />
+    <div className="row">
+      <div className="col-lg-8 mx-auto">
+        <Products />
+      </div>
     </div>
   );
 }
