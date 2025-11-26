80d82b0f5a18fb949565f03446a564f931edd62a
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-26 16:02:27 +0300

    update

--- pro-academy/src/Components/Button.js
+++ pro-academy/src/Components/Button.js
@@ -1,4 +1,10 @@
+import React from "react";
+
 const Button = (props) => {
-  return <button className="btn btn-primary">{props.children}</button>;
+  return (
+    <button onClick={props.eventHandler} className="btn btn-primary">
+      {props.children}
+    </button>
+  );
 };
 export default Button;
--- pro-academy/src/Components/ProductDetails.js
+++ pro-academy/src/Components/ProductDetails.js
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import React from "react";
 
 let productCount = 0;
 
@@ -14,14 +15,25 @@ let style = {
 const ProductDetails = (props) => {
   let badgeClass = "badge-margin badge ";
   badgeClass += props.isAvailable ? "bg-success" : "bg-danger";
+
+  let incrementProductCount = function () {
+    productCount++;
+    console.log(productCount);
+  };
+
+  let decrementProductCount = function () {
+    productCount--;
+    console.log(productCount);
+  };
+
   return (
     <div className="d-flex align-items-center justify-content-start mt-1">
       <h6 className="font-weight-bold my-2" style={{ "margin-right": 20 }}>
         {props.price}
       </h6>
-      <Button>-</Button>
+      <Button eventHandler={decrementProductCount}>-</Button>
       <span style={style}>{displayFormattedCount()}</span>
-      <Button>+</Button>
+      <Button eventHandler={incrementProductCount}>+</Button>
       <span className={badgeClass}>
         {props.isAvailable ? "Available" : "Unavailable"}
       </span>
