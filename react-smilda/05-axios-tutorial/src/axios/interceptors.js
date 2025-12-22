ecb812d77b0dac546233597115e89d9b8a8b2a7b
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-22 15:45:56 +0300

    updated

---
+++ react-smilda/05-axios-tutorial/src/axios/interceptors.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const authFetch = axios.create({
+  baseURL: "",
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers["Accept"] = "application/json";
+    console.log("request sent");
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.request.use(
+  (response) => {
+    console.log("response sent");
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+  }
+);
+
+export default authFetch;
