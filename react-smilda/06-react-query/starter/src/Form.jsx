45b928de5202df5cf72c12af9be987c4fda09746
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-22 21:15:25 +0300

    updated

--- react-smilda/06-react-query/starter/src/Form.jsx
+++ react-smilda/06-react-query/starter/src/Form.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react';
+import { Mutation, useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import customFetch from "./utils";
+import Title from "../../../05-axios-tutorial/src/components/Title";
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+  const [newItemName, setNewItemName] = useState("");
+
+  const result = useMutation({
+    mutationFn: () => customFetch.post("/", { title: "some title" }),
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -9,14 +16,14 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit}>
       <h4>task bud</h4>
-      <div className='form-control'>
+      <div className="form-control">
         <input
-          type='text '
-          className='form-input'
+          type="text "
+          className="form-input"
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        <button type="submit" className="btn">
           add task
         </button>
       </div>
