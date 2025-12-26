ff6b2e88d9404593e4688ee051128fdf56f20bce
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-26 17:46:44 +0300

    updated

--- react-smilda/07-unsplash-images/starter/src/Gallery.jsx
+++ react-smilda/07-unsplash-images/starter/src/Gallery.jsx
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useGlobalContext } from "./context";
 
-const url =
-  "https://api.unsplash.com/search/photos?client_id=2A256JVytac6w3r6u4k_fsDe8_8kZZPC36OTZ2bBHdY&query=office";
+const url = `https://api.unsplash.com/search/photos?client_id=2A256JVytac6w3r6u4k_fsDe8_8kZZPC36OTZ2bBHdY`;
 
 const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
   const response = useQuery({
-    queryKey: ["images"],
+    queryKey: ["images", searchTerm],
     queryFn: async () => {
-      const result = await axios.get(url);
+      const result = await axios.get(`${url}&query=${searchTerm}`);
       return result.data;
     },
   });
