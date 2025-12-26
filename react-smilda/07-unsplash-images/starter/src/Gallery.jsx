afd630b081fdf309c53d77a3dfd33f458d007303
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-26 16:22:02 +0300

    updated

--- react-smilda/07-unsplash-images/starter/src/Gallery.jsx
+++ react-smilda/07-unsplash-images/starter/src/Gallery.jsx
@@ -1,4 +1,57 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=2A256JVytac6w3r6u4k_fsDe8_8kZZPC36OTZ2bBHdY&query=office";
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  // console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found....</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
--- react-smilda/07-unsplash-images/starter/src/main.jsx
+++ react-smilda/07-unsplash-images/starter/src/main.jsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context";
-import {
-  QueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
