1870309464123a34de2b469c3179b4acdf0779ef
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-28 19:04:50 +0300

    updated

--- react-smilda/08-contentful-cms/starter/src/Projects.jsx
+++ react-smilda/08-contentful-cms/starter/src/Projects.jsx
@@ -1,4 +1,13 @@
+import { useFetchProjects } from "./fetchProjects";
+
 const Projects = () => {
+  const { loading, projects } = useFetchProjects;
+  console.log(loading, projects);
+  if (loading) {
+    <section className="projects">
+      <h2>Loading...</h2>
+    </section>;
+  }
   return <h2>Projects</h2>;
 };
 export default Projects;
