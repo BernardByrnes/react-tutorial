66657b59f16c7463d6139a49a70ecf0c32b63e6b
BernardByrnes <bernardtamboedu@gmail.com>
2026-01-13 21:33:56 +0300

    updated

--- typescript and React/vite-react-ts/src/App.tsx
+++ typescript and React/vite-react-ts/src/App.tsx
@@ -3,9 +3,9 @@ import Person from "./Person";
 
 let name: string = "pedro";
 let age: number = 45;
-let evens: number[] = [1, 2, 3, 4, 5, 6];
-let logged: boolean = false;
-console.log(name, age, evens, logged);
+// let evens: number[] = [1, 2, 3, 4, 5, 6];
+// let logged: boolean = false;
+console.log(name, age,);
 
 function App() {
   console.log(name);
