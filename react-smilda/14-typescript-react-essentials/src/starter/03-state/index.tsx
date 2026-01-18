76dd5efb85962ef6ff5d12beee958c5f63dbaf40
BernardByrnes <bernardtamboedu@gmail.com>
2026-01-18 20:24:27 +0300

    updated

--- react-smilda/14-typescript-react-essentials/src/starter/03-state/index.tsx
+++ react-smilda/14-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 
+const navLinks = [
+  {
+    id: 1,
+    url: "crazy url🤣",
+  },
+];
+
 function Component() {
-  useState();
+  const [text, setText] = useState("take a break");
+  const [numb, setNumb] = useState(7);
+  const [list, setList] = useState<string[]>([]);
   return (
     <div>
       <h2 className="mb-1">React and TS</h2>
-      <button className="btn btn-center">Click Me</button>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          setText("cow");
+          setNumb(3);
+          setList(["cat"]);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
