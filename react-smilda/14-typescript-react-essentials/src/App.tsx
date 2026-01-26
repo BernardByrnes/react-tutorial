1e4f0ea1e84a1c32992480fc315187004f6c4f87
BernardByrnes <bernardtamboedu@gmail.com>
2026-01-26 19:53:37 +0300

    updated

--- react-smilda/14-typescript-react-essentials/src/App.tsx
+++ react-smilda/14-typescript-react-essentials/src/App.tsx
@@ -1,9 +1,11 @@
-import Component from "./starter/04-events";
+import Component from "./final/05-challenge";
+// import Component from "./starter/04-events";
 
 function App() {
   return (
     <main>
-      <Component />
+      <Component type="basic" name="suzan" />
+      <Component type="advanced" name="anna" email="anna@gmail.com " />
     </main>
   );
 }
