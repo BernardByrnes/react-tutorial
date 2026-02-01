a86e7cff888c78f53e098b5af96ca3e3f9701366
BernardByrnes <bernardtamboedu@gmail.com>
2026-02-01 20:05:21 +0300

    updated

--- react-smilda/09-mixmaster/starter/src/pages/HomeLayout.jsx
+++ react-smilda/09-mixmaster/starter/src/pages/HomeLayout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isPageLoading = navigation.state === "loading";
+  const value = "some value";
   return (
     <>
       <Navbar />
       <section className="page">
+        {isPageLoading ? <div className="loading" /> : <Outlet />}
         <Outlet />
       </section>
     </>
