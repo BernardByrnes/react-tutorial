88205ae46f0c1bcad43a7d24262594588b81a936
BernardByrnes <bernardtamboedu@gmail.com>
2026-02-01 16:14:24 +0300

    updated

--- react-smilda/09-mixmaster/starter/src/pages/Landing.jsx
+++ react-smilda/09-mixmaster/starter/src/pages/Landing.jsx
@@ -1,6 +1,19 @@
-ex;
+import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+export const loader = async () => {
+  const searchTerm = "margarita";
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  console.log(response);
+  return { drinks: response.data.drinks, searchTerm };
+};
 
 const Landing = () => {
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <div>Landing</div>;
 };
 export default Landing;
