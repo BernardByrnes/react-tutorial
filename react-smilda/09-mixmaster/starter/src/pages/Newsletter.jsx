280f25ad680f2641515e9cfafd90e1d09c6cde4b
BernardByrnes <bernardtamboedu@gmail.com>
2026-02-04 08:51:32 +0300

    updated

--- react-smilda/09-mixmaster/starter/src/pages/Newsletter.jsx
+++ react-smilda/09-mixmaster/starter/src/pages/Newsletter.jsx
@@ -1,4 +1,55 @@
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        Our Newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="firstname" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="firstname"
+          id="firstname"
+          defaultValue="John"
+        />
+      </div>
+
+      <div className="form-row">
+        <label htmlFor="lastname" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastname"
+          id="lastname"
+          defaultValue="sammy"
+        />
+      </div>
+
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="johns@text.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 export default Newsletter;
