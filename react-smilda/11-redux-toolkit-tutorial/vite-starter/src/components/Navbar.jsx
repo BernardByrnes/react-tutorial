27795ab7497025ddd5c8ce7024ea0ad3eb394395
BernardByrnes <bernardtamboedu@gmail.com>
2026-02-08 18:51:46 +0300

    updated

---
+++ react-smilda/11-redux-toolkit-tutorial/vite-starter/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const amount = useSelector((store) => store.cart.amount);
+  return (
+    <div className="nav-center">
+      <h3>Redux toolkit</h3>
+      <div className="nav-container">
+        <CartIcon />
+        <div className="amount-container">
+          <p className="total-amount">{amount}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
